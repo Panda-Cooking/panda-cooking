e104f5ddcb3c4998b897d3fb7bee86e13f32acf1
Victor <Novakoski2021@gmail.com>
2022-10-31 16:26:01 -0300

    initial commit

---
+++ src/styles/Global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  :root{
+    --Color-brand-1:#ff8787
+    --Color-brand-2:#ffc9c9
+    --Color-brand-3:#fff5f5
+    --Color-gray-0:#f8f9fa
+    --Color-gray-1:#dee2e6
+    --Color-gray-2:#adb5bd
+    --Color-gray-3:#343a40
+  }
+
+  button{
+    cursor: pointer;
+  }
+`
