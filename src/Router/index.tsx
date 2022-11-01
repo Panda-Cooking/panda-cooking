4dd95a525c1e07c3a84faf5e4b466b1bed3a4e3b
Felipe <felipebbulhoes@gmail.com>
2022-11-01 15:30:57 -0300

    fix: sign conflicts

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -4,14 +4,14 @@ import Dashboard from "../components/Pages/Dashboard";
 import LandingPage from "../components/Pages/LandingPage";
 import RecipesPage from "../components/Pages/RecipesPage";
 import SignIn from "../components/Pages/SignIn";
-import SingUp from "../components/Pages/SingUp";
+import SignUp from "../components/Pages/SignUp";
 import RecipesSingUp from "../components/Pages/RecipesSingUp";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
-      <Route path="/signUp" element={<SingUp />} />
+      <Route path="/signUp" element={<SignUp />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/recipesSignUp" element={<RecipesSingUp />} />
       <Route path="/recipesPage" element={<RecipesPage />} />
