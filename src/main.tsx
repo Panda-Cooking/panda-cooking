6787cff09b240d7e4a0ae802e2e4d5e560b96a74
Victor <Novakoski2021@gmail.com>
2022-11-03 17:14:21 -0300

    fix:refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import UserProvider from "./contexts/UserContext";
-import AuthContext from './contexts/AuthContext';
-import RecipeProvider from './contexts/RecipesContext';
-import { GlobalStyle } from './styles/Global';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { GlobalStyle } from './styles/Global'
+import Providers from './contexts/Providers'
 
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthContext>
-        <RecipeProvider>
-         <UserProvider>
-          <GlobalStyle />
-          <App />
-         </UserProvider>
-        </RecipeProvider>
-      </AuthContext>
+      <Providers>
+        <GlobalStyle />
+        <App />
+      </Providers>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://panda-cooking-brasil.herokuapp.com",
-  timeout: 5000,
-});
-
-export default api;
